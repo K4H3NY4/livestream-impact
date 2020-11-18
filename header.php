c731e6daaec57409d304213dc6efde2e00d77974
John Kahenya <kahenyaa@gmail.com>
2020-11-19 02:16:36 +0300

    Add files via upload

---
+++ header.php
@@ -0,0 +1,205 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="utf-8" />
+	<meta http-equiv="X-UA-Compatible" content="IE=edge" />
+	<meta name="viewport" content="width=device-width, initial-scale=1" />
+	<meta name="description" content="" />
+	<meta name="author" content="" />
+	<meta http-equiv='cache-control' content='no-cache'> 
+<meta http-equiv='expires' content='0'> 
+<meta http-equiv='pragma' content='no-cache'> 
+
+
+	<link rel="preload" href="https://www.shapehistory.com/wp-content/themes/shape-history/assets/fonts/Lato-Regular.woff2" as="font"
+		type="font/woff2" crossorigin>
+	<link rel="preload" href="https://www.shapehistory.com/wp-content/themes/shape-history/assets/fonts/Lato-Light.woff2" as="font"
+		type="font/woff2" crossorigin>
+
+	
+<!-- Search Engine Optimization by Rank Math - https://s.rankmath.com/home -->
+<title>Livestream Impact</title>
+
+
+<link rel='stylesheet' id='shape-history-typekit-css'  href='https://use.typekit.net/cbv3swv.css' />
+<link rel='stylesheet' id='shape-history-swiper-css'  href='wp-content/themes/shape-history/assets/css/swiper-bundle.min.css' />
+<link rel="stylesheet" href="wp-content/themes/shape-history/assets/css/swiper-bundle.min.css"> 
+<link rel="stylesheet" href="wp-content/themes/shape-history/assets/css/app.min.css"> 
+<link rel='stylesheet' id='shape-history-style-css'  href='wp-content/themes/shape-history/style.css' />
+
+<style>
+
+.svg-img {
+ 
+ filter: saturate(100);
+ 
+}
+
+.title{
+	color:#ed1c24;
+}
+</style>
+
+
+
+<body 
+	class="home page-template page-template-templates page-template-home page-template-templateshome-php page page-id-111">
+	<div class="theme-app">
+		<!-- <a name="gotop" id="gotop"></a> -->
+		<div class="c-header c-header-fullscreen" id="scrollTop">
+			<header id="header" class="theme-header">
+				<div class="overlay-contactWrapper">
+							  <div class="overlay-contactForm">
+		  <div class="body-overlay d-none-1"></div>
+		    <div class="contact-container__nav d-none d-md-block">
+		      <div class="circle-arrow">
+		        <a href="" class="js-closeform">
+		        	<svg id="_200520_shape_history_website_circle_with_arrow" data-name="200520_shape_history_website_circle_with_arrow"
+    xmlns="http://www.w3.org/2000/svg" width="500" height="500" viewBox="0 0 500 500">
+    <g id="Group_135" data-name="Group 135">
+        <circle id="Ellipse_13" data-name="Ellipse 13" cx="250" cy="250" r="250" fill="#ffab00" />
+    </g>
+    <path id="Path_896" data-name="Path 896"
+        d="M267.23,212.56c14.47,12.12,30,22.82,45.07,34.13-53.03-.48-106.04-.24-159.08-.35-8.19-.02-8.19,12.68,0,12.7,51.59.11,103.14-.25,154.72.3-14.93,9.45-29.37,19.68-45.07,27.89-7.26,3.79-.84,14.75,6.41,10.96,22.5-11.76,42.4-27.73,64.89-39.5,3.38-1.77,4.41-7.43,1.28-9.97-19.28-15.68-40.19-29.18-59.25-45.14C269.98,198.36,260.95,207.29,267.23,212.56Z"
+        fill="#434c57" />
+</svg>		          
+		        </a>
+		      </div>
+		      <div class="close">
+		        <a href="" class="js-closeform">
+				<img src="wp-content/themes/shape-history/svg/images/cross.svg" class="svg-img" alt="Shape History Hero" width="100%"
+          height="100%" />
+		          
+		        </a>
+		      </div>
+		    </div>
+		    <div class="contact-container">
+		      <div class="overlay-container js-closeform"></div>
+		      <div class="contact-container__form">
+		        <header>
+		          <h3 class="text-primary">WHERE TO FIND US</h3>
+		          <div class="contact-container__caption">
+                  
+                    
+                    
+Livestream Impact Ltd <br><br>
+
+Tel: +254 733 624 356 <br><br>
+
+E-mail: info@livestreamimpact.com <br><br>
+
+
+		          </div>
+		        </header>
+		       			
+		     
+		      </div>
+		    </div>
+		  </div>				</div>
+				<!-- END OF contact overlay form -->
+								<div class="main-navigation">
+                 
+					<div class="offcanvas-container">
+         
+						<div class="offcanvas-container__top">
+              
+							<div class="container" style="z-index: 9999;">
+
+								<div class="row">
+                 
+									<div class="col-md-6 d-none d-md-block" style="z-index: 9999;">
+										<div class="offcanvas-container__top-left">
+											<figure class="sh-logo-large__white">
+												<a href="">
+													<img src="wp-content/uploads/2020/06/sh_logo_large-white@2x.png" class="img-fluid"
+														alt="Shape History White logo" width="100%" height="100%" />
+															<br>
+														
+												</a>
+											</figure>
+										</div>
+									</div>
+									<div class="col-md-6" style="z-index: 9999;">
+										<div class="offcanvas-container__top-right">
+											<div class="main-menu">
+												<ul id="menu-main-menu" class="ul-nolist"><li class='main-menu--list home-nav menu-item menu-item-type-post_type menu-item-object-page menu-item-home current-menu-item page_item page-item-111 current_page_item'><a  href="index.php" 
+												onclick="window.location.href='index.php';"
+												><span class="page-name">Home</span><span class="page-name-caption"></span></a></li></li>
+								<li class='main-menu--list  menu-item menu-item-type-post_type menu-item-object-page'><a  href="index.php#about" onclick="window.location.href='index.php#about';"><span class="page-name">ABOUT US</span><span class="page-name-caption">Expertise</span></a></li></li>
+								<li class='main-menu--list  menu-item menu-item-type-post_type menu-item-object-page'><a  href="index.php#work" onclick="window.location.href='index.php#work';"><span class="page-name">WORK</span><span class="page-name-caption">Impact we've driven</span></a></li></li>
+								<li class='main-menu--list  menu-item menu-item-type-post_type menu-item-object-page'><a  href="index.php#team" onclick="window.location.href='index.php#team';"><span class="page-name">TEAM</span><span class="page-name-caption">We make it happen</span></a></li></li>
+								<li class='main-menu--list js-contact-trigger menu-item menu-item-type-custom menu-item-object-custom'><a  href="#!"><span class="page-name">Contact</span><span class="page-name-caption">Ready to talk</span></a></li></li>
+								</ul>											</div>
+										</div>
+									</div>
+								</div>
+							</div>
+
+            </div>
+           
+            <!-- End of Offcanvas top container -->
+            
+												<div class="offcanvas-container__bottom">
+                          
+							<div class="container fixed-bottom pb-5 mb-5" >
+								<div class="row justify-content-center justify-content-md-start">
+																		<div class="col-2 col-lg-1 offset-md-2 offset-lg-4">
+										<a class="main-navigation--sociallink" target="_blank"
+											href="https://twitter.com/">
+											Twitter										</a>
+									</div>
+																		<div class="col-2 col-lg-1 ">
+										<a class="main-navigation--sociallink" target="_blank"
+											href="https://www.instagram.com/">
+											Instagram										</a>
+									</div>
+																		<div class="col-2 col-lg-1 ">
+										<a class="main-navigation--sociallink" target="_blank"
+											href="https://www.linkedin.com/company/">
+											Linkedin										</a>
+									</div>
+																		<div class="col-2 col-lg-1 ">
+										<a class="main-navigation--sociallink" target="_blank"
+											href="https://www.facebook.com/">
+											Facebook										</a>
+                  </div>
+                
+                                  </div>
+                                 
+              </div>
+              <video src=""  autoplay style="opacity: 20%;     top: 90px;
+              position: relative;" autoplay loop></video>
+            </div>
+          
+						<!-- End of  Offcanvas Bottom container -->
+					</div>
+					<!-- Endof ot Offcanvas Menu-->
+				</div>
+				<!-- <div class="theme-header__logoWrapper"> -->
+
+                <div class="theme-header__logoWrapper js-logo-showhide animate">
+					<a href="index.php" title="Shape History" class="brand-logo">
+						<img src="wp-content/themes/shape-history/svg/images/menu-logo-line.svg?ver=1604998367"
+							class="svg-img menu-logo-line" alt="Shape History Hero" width="100%" height="100%" />
+
+						<img src="img/image--081.png" alt="Brand logo" class="img-fluid bright-theme-logo" />
+						<img src="img/image--081.png" alt="Brand logo" class="img-fluid dark-theme-logo" />
+					</a>
+				</div>
+				<div class="theme-header__hamburgerWrapper">
+					<div id="hamburger" class="theme-header__menu js-navigation-trigger">
+						<div class="js-hamwrap">
+							<img src="wp-content/themes/shape-history/svg/images/ham.svg?ver=1604998367"
+								id="hamburger" class="svg-img" alt="Shape History Hero" width="100%" height="100%" />
+
+						</div>
+						<div class="js-closewrap">
+							<img src="wp-content/themes/shape-history/svg/images/close.svg?ver=1604998367"
+								id="close" class="svg-img" alt="Shape History Hero" width="100%" height="100%" />
+						</div>
+					</div>
+				</div>
+			</header>
+		</div>
+		<div class="theme-wrap overflow-hidden" data-barba="wrapper">
---
+++ nambwa.php
@@ -0,0 +1,250 @@
+<?php
+include('header.php');
+
+?>
+			<div class="c-container-change" data-barba="container" data-barba-namespace="single"
+				data-scroll-content><main class="theme-main">
+
+	<section class="above-the-fold">
+		<div class="hero-container-caseStudy position-relative">
+						<figure >
+				<img src="img/image--030.jpg" class="img-fluid d-none d-md-block" alt="The Elders" width="100%" height="100%" />
+				<img src=" img/image--030.jpg" class="d-block d-md-none img-fluid" alt=" The Elders" width="100%" height="100%" />
+			</figure>	
+			<div class="hero-counters">
+					<div class="container">
+						<div class="row count-this">
+															<div class="col-md-10 col-lg-10 offset-lg-1 col-counter translateY-o--100 js-fristenter enteredonce entered">
+									<div class="hero-counter hero-counter__people text-center">
+										<h4 class="counter">OUR WORLD OF SOCIAL CHANGE</h4>
+										<p>HARNESSING THE POWER OF COMMUNICATIONS TO CHANGE THE WORLD</p>
+									</div>
+								</div>
+					    					
+					    							</div>
+					</div>
+				</div>
+					</div>
+	</section>
+	<!-- END of above the fold -->
+
+	<section class="theme-section-14">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-10 offset-lg-1">
+					<h6 class="title translateY-100"> KENYA NAMBWA FM FEATURE FILM</h6>
+				</div>
+			</div>
+			<!-- post data-->
+						<div class="row  translateY-100 case-study-wrapper">
+				<div class="col-md-9 col-xxl-3 offset-lg-3">
+					<span class="case-meta">Terre Des Hommes Netherlands</span>					
+				</div>
+			</div>
+							<div class="row">
+					<div class="col-md-8 offset-lg-3">
+						<div class="case-study-content">
+							<h2 class="translateY-100">
+								<span class="header">
+								A youth-led advocacy edutainment social change campaign to implement a multimedia tailored communication solution anchored on a
+feature length film [Nambwa FM]. Implemented over a period of 8 months from research, development, production, monitoring and
+evaluation, the campaign is to create awareness on gender based violence and economic empowerment in Kwale County.</span>
+							</h2>
+						</div>
+					</div>
+				</div>
+			
+					
+			
+						</div>
+	</section>
+
+	<!-- END OF section case study content -->
+
+
+				<section class="case-study-card case-study-card-1 ">
+				<div class="container">
+				<!-- BEGIN OF Case study -->
+					
+											<div class="row d-flex align-items-end">
+							<div class="col-md-3 col-lg-2 card-body offset-lg-1">
+								<div class="case-study-card-body  translateY-100">
+									<h3>
+										Campaign flagship video									</h3>
+									<p>
+																			</p>
+								</div>
+							</div>
+					
+						</div>
+						
+				</div>
+			</section>
+		<!-- END of case study card 1 -->
+						<section class="case-study-card case-study-card-2 ">
+				<div class="container">
+				<!-- BEGIN OF Case study -->
+					
+											<div class="row">
+							<div class="col-10 col-md-6 col-lg-5 offset-lg-1">
+																	<figure class="card-img-top  translateY-100">
+										<img src="img/image--031.jpg" class="img-fluid" alt="Shape History Hero" width="100%"
+											height="100%" />
+									</figure>
+																</div>
+							<div class="col-10 col-md-6 col-lg-5 ml-auto ml-md-0 card-img-bottom">
+                <img src="img/image--032.jpg" class="img-fluid" alt="Shape History Hero" width="100%"
+                height="100%" />
+																	</div>
+						</div>
+					
+				</div>
+			</section>
+		<!-- END of case study card 1 -->
+						<section class="case-study-card case-study-card-1 ">
+				<div class="container">
+				<!-- BEGIN OF Case study -->
+					
+											<div class="row d-flex align-items-end">
+							<div class="col-md-3 col-lg-2 card-body offset-lg-1">
+								<div class="case-study-card-body  translateY-100">
+									<h3>
+																			</h3>
+									<p>
+																			</p>
+								</div>
+							</div>
+							<div class="col-md-9 col-lg-8 card-figure translateY-100">
+                <img src="img/image--030.jpg" class="img-fluid" alt="Shape History Hero" width="100%"
+                height="100%" />
+																	</div>
+						</div>
+						
+				</div>
+			</section>
+		<!-- END of case study card 1 -->
+						<section class="case-study-card case-study-card-2 ">
+				<div class="container">
+				<!-- BEGIN OF Case study -->
+					
+											<div class="row">
+							<div class="col-10 col-md-6 col-lg-5 offset-lg-1">
+																	<figure class="card-img-top  translateY-100">
+										<img src="img/image--032.jpg" class="img-fluid" alt="Shape History Hero" width="100%"
+											height="100%" />
+									</figure>
+																</div>
+							<div class="col-10 col-md-6 col-lg-5 ml-auto ml-md-0 card-img-bottom">
+                <img src="img/image--031.jpg" class="img-fluid" alt="Shape History Hero" width="100%"
+                height="100%" />
+																	</div>
+						</div>
+					
+				</div>
+			</section>
+		<!-- END of case study card 1 -->
+						<section class="case-study-card case-study-card-1 reverse">
+				<div class="container">
+				<!-- BEGIN OF Case study -->
+					
+											<div class="row d-flex align-items-end">
+							<div class="col-md-3 col-lg-2 card-body offset-lg-1">
+								<div class="case-study-card-body  translateY-100">
+									<h3>
+										Marching with the Elders in key cities around the world 									</h3>
+									<p>
+																			</p>
+								</div>
+							</div>
+							<div class="col-md-9 col-lg-8 card-figure translateY-100">
+																	<figure>
+										<img src="img/image--030.jpg" class="img-fluid" alt="" width="100%"
+											height="100%" />
+									</figure>
+															</div>
+						</div>
+						
+				</div>
+			</section>
+		<!-- END of case study card 1 -->
+						<section class="case-study-card case-study-card-2 ">
+				<div class="container">
+				<!-- BEGIN OF Case study -->
+					
+											<div class="row">
+							<div class="col-10 col-md-6 col-lg-5 offset-lg-1">
+                <img src="img/image--031.jpg" class="img-fluid" alt="Shape History Hero" width="100%"
+                height="100%" />
+																	</div>
+							<div class="col-10 col-md-6 col-lg-5 ml-auto ml-md-0 card-img-bottom">
+																	<figure class="translateY-100">
+										<img src="img/image--030.jpg" class="img-fluid" alt="Shape History Hero" width="100%"
+											height="100%" />
+									</figure>
+															</div>
+						</div>
+					
+				</div>
+			</section>
+		<!-- END of case study card 1 -->
+			
+	<section class="case-study-pagination">
+		<div class="container">
+			<div class="row article-content-navigation__article  translateY-100">
+				<div class="col-3 col-md-4 col-lg-2 offset-lg-1">
+					
+							<a href="projects/unfe/" class="link link--action link--action-with-arrow__left">
+								<div class="arrow arrow-prev">
+									<svg xmlns="http://www.w3.org/2000/svg" width="30" height="16" viewBox="0 0 30 16">
+    <path id="_200520_shape_history_website_arrow_right" data-name="200520_shape_history_website_arrow_right" d="M19.934,1.74c2.279,2,4.725,3.765,7.1,5.631-8.354-.078-16.7-.039-25.06-.058a1.047,1.047,0,0,0,0,2.092c8.127.018,16.248-.042,24.373.05-2.351,1.559-4.627,3.247-7.1,4.6a1.037,1.037,0,0,0,1.009,1.807c3.544-1.941,6.678-4.577,10.222-6.515a1.05,1.05,0,0,0,.487-.77,1.077,1.077,0,0,0-.285-.874C27.645,5.119,24.351,2.892,21.349.252,20.362-.6,18.943.871,19.934,1.74Z" transform="translate(30.975 15.994) rotate(180)"/>
+  </svg>									
+								</div>
+								<span>previous project</span>
+							</a>
+									</div>
+				<div class="col-5 col-md-4 col-lg-6 text-center">
+					<a class="gotop link link--action">back to top</a>
+				</div>
+								<div class="col-4 col-md-4 col-lg-2">
+					<a href="projects/fondation-botnar/" class="next-article">
+						<span class="link link--action link--action-with-arrow__right">
+						  <span>next project</span>
+						</span>
+						<div class="arrow arrow-next">
+							<svg xmlns="http://www.w3.org/2000/svg" width="24" height="12.233" viewBox="0 0 24 12.233">
+  <path id="_200520_shape_history_website_arrow_right" data-name="200520_shape_history_website_arrow_right"
+    d="M16.157,1.329c1.825,1.529,3.784,2.879,5.686,4.306-6.69-.06-13.377-.03-20.068-.044a.8.8,0,0,0,0,1.6c6.508.014,13.011-.032,19.518.038-1.883,1.192-3.705,2.483-5.685,3.519-.915.478-.106,1.861.808,1.382,2.838-1.484,5.348-3.5,8.186-4.982a.8.8,0,0,0,.162-1.257c-2.432-1.978-5.07-3.681-7.474-5.7C16.5-.462,15.364.665,16.157,1.329Z"
+    transform="translate(-1 0.002)" fill="" />
+</svg>						</div>
+					  </a>
+				</div>
+							</div>
+			<!-- END of article navigation -->
+
+			<div class="row article-content-navigation__page justify-content-center  translateY-100">
+				<div class="col-md-10">
+					<div class="linkwrapper d-flex justify-content-between">
+						<div class="link-1 back-work">
+							<h3 class="h1 text-thin text-primary text-uppercase mb-0 mb-md-3">back to Work</h3>
+							<a href="work/" class="link link--action">click here</a>
+						</div>
+						<div class="link-2 back-expertise">
+							<h3 class="h1 text-thin text-primary text-uppercase mb-0 mb-md-3">go to expertise</h3>
+							<a href="expertise" class="link link--action">click here</a>
+						</div>
+					</div>
+
+				</div>
+			</div>
+		</div>
+	</section>
+
+</main> 
+
+
+
+
+<?php
+ include("footer.php");
+ exit();
+ ?>
